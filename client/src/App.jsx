4093452751a98b215142faa98b8150b4c63c2d21
jhoncarter7 <rainikhil044@gmail.com>
2023-11-11 20:50:39 +0530

    create header and add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 
+import { BrowserRouter,  Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+
 
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <BrowserRouter>
+   <Header/>
+   <Routes>
+    <Route path="/*" element={<Home/>}/>
+   </Routes>
+   
+   </BrowserRouter>
   )
 }
