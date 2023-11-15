07af9d8efd5a53f96db505e17899a76c169c799d
jhoncarter7 <rainikhil044@gmail.com>
2023-11-15 10:25:17 +0530

    add error-handler functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.models.js";
 import bcrypt from "bcrypt";
-import jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 export const signUp = async (req, res, next) => {
   const { fullName, email, mobileNo, password } = await req.body;
   const hashPassword = bcrypt.hashSync(password, 10);
@@ -14,23 +15,24 @@ export const signUp = async (req, res, next) => {
     await newUser.save();
     res.status(201).json("user created successfully");
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
 
-export const signIn = async (req, res, next)=>{
-  const {email, password} = req.body
-   try {
-    const validUser = await User.findOne({email})
-    if(!validUser) return console.log('user not Found')
-    const validPassword =  bcrypt.compareSync(password, validUser.password)
-  if(!validPassword) return console.log('wrong credential');
-    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
-    console.log(validUser)
-    const {password: pass, ...rest} = validUser._doc
-    res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest)
-   } catch (error) {
-    console.log(error)
-   }
-
-}
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credential"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import AuthRouter from './routes/auth.routes.js'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
     console.log('connected to database');
@@ -11,8 +12,19 @@ mongoose.connect(process.env.MONGODB_URL).then(()=>{
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 
 app.listen(3000, ()=>{
     console.log('server is running om port 3000')
 })
 app.use('/api/auth/', AuthRouter)
+
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error'
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
