bd158aba4698158ade879d83a2a1e404ff9095c4
jhoncarter7 <rainikhil044@gmail.com>
2023-11-15 10:26:47 +0530

    complete signup functionality & dynamic routes

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,41 @@
+import {  useState } from 'react';
 import {Link} from 'react-router-dom'
 
 export default function SignUp() {
+ const [userData, setUserData] = useState(null)
+
+ const changeHandler = (e) =>{
+  setUserData((prev)=>({...prev, [e.target.id]:e.target.value}))
+ }
+  const signupHandler = async (e) => {
+  e.preventDefault()
+    try {
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': "application/json"
+        },
+        body: JSON.stringify(userData)
+      })
+      const data = await res.json()
+      if(data.success === false){
+         console.log(data.message)
+         return;
+      }
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
       <div className="p-6 mt-16 max-w-xl mx-auto shadow-md rounded-lg">
         <h1 className="font-semibold text-slate-700 text-xl text-center">Signup </h1>
         <p className="text-slate-700 text-center">Get access to your Orders, Wishlist and Recommendations</p>
-        <form className="flex flex-col gap-4  items-center text-center p-5 sm:px-9 max-w-xl ">
-          <input type="usename" className="border rounded-lg p-2 px-5 outline-none text-slate-700 w-full" placeholder="your username"/>
-          <input type="email" className="border rounded-lg p-2 px-5 outline-none text-slate-700 w-full" placeholder="your email"/>
-          <input type="number" className="border rounded-lg p-2 px-5 outline-none text-slate-700 w-full" placeholder="Enter Mobile Number"/>
-          <input type="password" className="border rounded-lg p-2 px-5 outline-none text-slate-700 w-full" placeholder="your password"/>
+        <form onSubmit={signupHandler} className="flex flex-col gap-4  items-center text-center p-5 sm:px-9 max-w-xl ">
+          <input type='text' id="fullName" className="border rounded-lg p-2 px-5 outline-none text-slate-700 w-full" placeholder="your username" onChange={changeHandler}/>
+          <input type='email' id="email" className="border rounded-lg p-2 px-5 outline-none text-slate-700 w-full" placeholder="your email" onChange={changeHandler}/>
+          <input type='number' id="mobileNo" className="border rounded-lg p-2 px-5 outline-none text-slate-700 w-full" placeholder="Enter Mobile Number" onChange={changeHandler}/>
+          <input type='password' id="password" className="border rounded-lg p-2 px-5 outline-none text-slate-700 w-full" placeholder="your password" onChange={changeHandler}/>
           <button className="border p-4 bg-green-700 w-full rounded-lg uppercase text-white hover:opacity-90">sign up</button>
         </form>
         <div className=" flex gap-3 w-full  px-5 sm:px-9">
