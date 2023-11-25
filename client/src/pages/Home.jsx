68e0b6c53bc8b2393bcb2233c0531dc4f9b5b681
jhoncarter7 <rainikhil044@gmail.com>
2023-11-25 19:18:12 +0530

    complete banner

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,8 +9,8 @@ export default function Home() {
   const [isvisible, setisVisible] = useState(true)
   return (
     <main className="p-5">
-      <section className="md:flex gap-4 ">
-        <aside className="">
+      <section className="flex flex-col md:flex-row gap-4 ">
+        <aside className=" min-w-fit">
           <div className="flex items-center gap-4  p-4 px-6 bg-lime-600 text-white font-semibold sm:text-lg">
           <RxHamburgerMenu/>
             <h2 className="pr-5 ">
@@ -18,7 +18,7 @@ export default function Home() {
             </h2>
             <IoIosArrowDown onClick={()=>setisVisible((prev) => !prev)} className="cursor-pointer"/>
           </div>
-          <div hidden={true} className=" p-4 px-8 flex flex-col gap-2 sm:gap-3 font-medium text-sm sm:text-lg border " style={{visibility:`${isvisible? 'visible': 'hidden'}`, transform:`translateY(${isvisible ? '100':'0'})`, transition: "transform 2s ease"}}>
+          <div hidden={true} className=" p-4 px-8  flex-col gap-2 sm:gap-3 font-medium text-sm sm:text-lg border " style={{display:`${isvisible? 'flex': 'none'}`, transform:`translateY(${isvisible ? '100':'0'})`, transition: "transform 2s ease"}}>
             <h3 className="cursor-pointer  hover:translate-x-2 hover:transition-all ">
               Vegitables
             </h3>
@@ -39,8 +39,9 @@ export default function Home() {
             </h3>
           </div>
         </aside>
-        <div>
-          <h1> </h1>
+        <div className="h-[80vh] overflow-hidden w-[100%]">
+          <img className="max-h-fit " src='/public/image/banner.jpg' alt="fresh vegitable in your area" />
+          
           <button> </button>
         </div>
       </section>
