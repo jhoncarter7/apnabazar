7fb550775dff85857b7169a44f0ef30f444480c8
jhoncarter7 <rainikhil044@gmail.com>
2023-12-23 23:56:47 +0530

    create editProduct

--- client/src/pages/admin/ProductList.jsx
+++ client/src/pages/admin/ProductList.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import Dashboard from "../../components/Dashboard";
-import ProductsCard from "../../components/ProductsCard";
 import { useSelector } from "react-redux";
 
 export default function ProductList() {
@@ -26,7 +25,23 @@ export default function ProductList() {
     <main className="flex justify-between gap-3 md:gap-8">
       <Dashboard className='self-start'/>
      <div  className="w-full grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 overflow-hidden gap-y-4">
-      {productLists?  productLists.map((product)=> <ProductsCard key={product._id} title={product.title} oldPrice={product.oldPrice} newPrice={product.newPrice} imageUrl={product.imageUrl}/>): <h4>No Product Listed!</h4>}
+      {productLists?  productLists.map((product)=>   
+      <div className="w-[65vw]  md:w-[42vw] lg:w-[18vw] shadow-md p-[2vw] rounded-md" key={product._id}>
+      <img className="h-[26vh]" src={product.imageUrl[0]} alt="" />
+      <p className="text-xs text-gray-700">6 mins</p>
+      <h3 className="text-sm">{product.title} </h3>
+      <p className="text-gray-600 font-light">500g</p>
+      <div className="flex justify-between">
+        <div>
+          <p className="text-xs">{product.newPrice}</p>
+          <p className="text-xs text-gray-600 line-through items-center">{product.oldPrice}</p>
+        </div>
+        <button className="border-2 px-8 border-green-500 rounded-md bg-green-100 text-green-500 font-semibold cursor-pointer">EDIT</button>
+      </div>
+    </div>
+      
+      ): <h4>No Product Listed!</h4>
+      }
      </div>
     </main>
   )
