85ca66521e4c4c044937b48d4b06f1ac6c0b1fa6
jhoncarter7 <rainikhil044@gmail.com>
2024-02-26 14:23:21 +0530

    Update category names and add shopping cart icon

--- client/src/components/AllCategories.jsx
+++ client/src/components/AllCategories.jsx
@@ -6,59 +6,59 @@ export default function AllCategories() {
     <div className="flex flex-wrap gap-4">   
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer"  to={'/vegetable-fruits'}> 
     <img className="w-[10vw]" src="/image/fruits&veg.png" alt="Vegetables & Fruits" />   
-    <p>Vegetables & Fruits</p>
+    <p className='text-xs sm:text-sm md:text-base'>Vegetables & Fruits</p>
     </Link>
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/dary-Breakfast"} > 
     <img className="w-[10vw]"  src="/image/dairy.avif" alt="Dairy & Breakfast" />   
-    <p>Dairy & Breakfast</p>
+    <p className='text-xs sm:text-sm md:text-base'>Dairy & Breakfast</p>
     </Link>
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/munchies"} > 
     <img className="w-[10vw]" src="/image/munchies.avif" alt="Munchies" />   
-    <p>Munchies</p>
+    <p className='text-xs sm:text-sm md:text-base'>Munchies</p>
     </Link>
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/cold-Drink"} > 
     <img className="w-[10vw]" src="/image/coldring.avif" alt="Cold Drinks & Juices" />   
-    <p>Cold Drinks & Juices</p>
+    <p className='text-xs sm:text-sm md:text-base'>Cold Drinks & Juices</p>
     </Link>
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/instant-frozen"} > 
     <img className="w-[10vw]" src="/image/instant.avif" alt="Instant & Frozen Food" />   
-    <p>Instant & Frozen Food</p>
+    <p className='text-xs sm:text-sm md:text-base'>Instant & Frozen Food</p>
     </Link>
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/tea-coffe"} > 
     <img className="w-[10vw]" src="/image/tea.avif" alt="Tea, coffee & Health Drinks" />   
-    <p>Tea, coffee & Health Drinks</p>
+    <p className='text-xs sm:text-sm md:text-base'>Tea, coffee & Health Drinks</p>
     </Link>
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/bakery"} > 
     <img className="w-[10vw]" src="/image/bakery.avif" alt="Bakery & Biscuits" />   
-    <p>Bakery & Biscuits</p>
+    <p className='text-xs sm:text-sm md:text-base'>Bakery & Biscuits</p>
     </Link>
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/sweet-tooth"} > 
     <img className="w-[10vw]" src="/image/sweetTooth.avif" alt="" />   
-    <p>Sweet Tooth</p>
+    <p className='text-xs sm:text-sm md:text-base'>Sweet Tooth</p>
     </Link>
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/atta-Rice"}> 
     <img className="w-[10vw]" src="/image/atta.avif" alt="" />   
-    <p>Atta, Rice & Dal</p>
-    </Link>
+    <p className='text-xs sm:text-sm md:text-base'>Atta, Rice & Dal</p>
+    </Link> 
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/dry-Fruits"} > 
     <img className="w-[10vw]" src="/image/mashala.avif" alt="" />   
-    <p>Dry Fruits, Masala & oil</p>
+    <p className='text-xs sm:text-sm md:text-base'>Dry Fruits, Masala & oil</p>
     </Link>
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/sauces-spreads"} > 
     <img className="w-[10vw]" src="/image/sauces.avif" alt="" />   
-    <p>Sauces & Spreads</p>
+    <p className='text-xs sm:text-sm md:text-base'>Sauces & Spreads</p>
     </Link>
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/baby-Care"} > 
     <img className="w-[10vw]" src="/image/baby.avif" alt="" />   
-    <p>Baby Care</p>
+    <p className='text-xs sm:text-sm md:text-base'>Baby Care</p>
     </Link>
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/cleaning"} > 
     <img className="w-[10vw]" src="/image/cleaning.avif" alt="" />   
-    <p>Cleaning Essentials</p>
+    <p className='text-xs sm:text-sm md:text-base'>Cleaning Essentials</p>
     </Link>
     <Link className="w-[12vw] h-45 min-w-[6rem] cursor-pointer" to={"/personal-care"} > 
     <img className="w-[10vw]" src="/image/personalcare.avif" alt="" />   
-    <p>Personal Care</p>
+    <p className='text-xs sm:text-sm md:text-base'>Personal Care</p>
     </Link>
     
     </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { IoIosArrowDown } from "react-icons/io";
 import { useState } from "react";
 import AllCategories from "../components/AllCategories";
-
-
+import { PiShoppingCartBold} from 'react-icons/pi'
+import {Link} from "react-router-dom"
 
 
 export default function Home() {
@@ -13,11 +13,14 @@ export default function Home() {
       <section className="flex flex-col md:flex-row gap-4 ">
         <aside className=" min-w-fit">
           <div className="flex items-center gap-4  p-4 px-6 bg-lime-600 text-white font-semibold sm:text-lg">
-          <RxHamburgerMenu/>
-            <h2 className="pr-5 ">
+          <RxHamburgerMenu className="cursor-pointer" onClick={()=>setisVisible((prev) => !prev)}/>
+            <h2 className="pr-1 ">
               All departmant
             </h2>
             <IoIosArrowDown onClick={()=>setisVisible((prev) => !prev)} className="cursor-pointer"/>
+            <Link to={'/admin/viewcart'} className='text-lg md:text-2xl ml-auto'> 
+            <PiShoppingCartBold />
+            </Link>
           </div>
           <div hidden={true} className=" p-4 px-8  flex-col gap-2 sm:gap-3 font-medium text-sm sm:text-lg border " style={{display:`${isvisible? 'flex': 'none'}`, transform:`translateY(${isvisible ? '100':'0'})`, transition: "transform 2s ease"}}>
             <h3 className="cursor-pointer  hover:translate-x-2 hover:transition-all ">
@@ -39,6 +42,7 @@ export default function Home() {
               Vegitables
             </h3>
           </div>
+
         </aside>
         <div className="md:h-[50vh] lg:h-[80vh] overflow-hidden w-[100%]">
           <img className="max-h-fit " src='/image/banner.jpg' alt="fresh vegitable in your area" />
