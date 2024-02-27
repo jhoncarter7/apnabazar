665c51bf0aa9af4b00daf66bcb6a78fffa60d4c8
jhoncarter7 <rainikhil044@gmail.com>
2024-02-27 22:04:06 +0530

    Add static file serving for client and fallback route to index.html

--- api/index.js
+++ api/index.js
@@ -22,12 +22,17 @@ app.use(cors({
     credentials: true
 }))
 
+const __dirname = path.resolve()
+
 app.listen(3000, ()=>{
     console.log('server is running om port 3000')
 })
 app.use('/api/auth/', AuthRouter)
 app.use('/api/', productRoute)
-
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal server error'
